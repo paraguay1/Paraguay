3367bc0127f2cfade07642c73ee3f8f58af9440b
joao <113110004+paraguay1@users.noreply.github.com>
2024-09-27 11:17:41 -0300

    js

--- script.js
+++ script.js
@@ -25,20 +25,31 @@ const perguntas = [
                     ]
             },
 ];
-let atual = 0;
+
+let atual 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
-     perguntaAtual = perguntas[atual];
-     caixaPerguntas.textContent = perguntaAtual.enunciado;
-     mostraAlternativas ()
+     perguntaAtual perguntas [atual];
+     caixaPerguntas.textContent perguntaAtual.enunciado;
+     mostraAlternativas();
 }
-    
+
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-    const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa;
-    caixaAlternativas.appendChild(botaoAlternativas)
-     }
- }
-mostraPergunta()
+    for(fconst alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas document.createElement("button");
+        botaoAlternativas.text.Content alternativa.texto;
+        botaoAlternativas.addEventListener("click", () respostaselecionada(alternativa)); 
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada (opcaoSelecionada) [
+     const afirmacoes opcaoSelecionada.afirmacoes;
+     historiaFinal afirmacoes;
+     atual++;
+     mostraPergunta();
+}
+
+mostraPergunta();
